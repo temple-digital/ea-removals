045a6a4836bf3ba7236bd75dca315716f71b454e
lewiswilsonn <lewiswilsonn@yahoo.co.uk>
2019-06-21 16:42:02 +0100

    update base functions

--- wp-content/themes/nidev/functions.php
+++ wp-content/themes/nidev/functions.php
@@ -1,34 +1,19 @@
 <?php
 
-/*	 Enqueue scripts and styles.	*/
+function custom_scripts() {
+	wp_enqueue_style( 'global-css', get_template_directory_uri() . '/css/global.css' );
+	wp_enqueue_script( 'global-js', get_theme_file_uri( '/js/global.js' ), array( 'jquery' ), '1.0', true );
+}
 
- function custom_scripts() {
-
-	 // Theme stylesheet.
-	 wp_enqueue_style( 'global-css', get_template_directory_uri() . '/css/global.css' );
-
-	 // Theme Scripts.
-	 wp_enqueue_script( 'global-js', get_theme_file_uri( '/assets/js/global.js' ), array( 'jquery' ), '1.0', true );
-
- }
-
- // Add jQuery
-function urframework_enqueue_jquery() {
+//jQuery register from theme files
+function enqueue_jquery() {
     wp_deregister_script('jquery');
     wp_register_script('jquery', get_template_directory_uri() . '/js/jquery-3.1.0.min.js');
     wp_enqueue_script('jquery');
 }
 
-function urframework_enqueue_global() {
-    wp_enqueue_script('globaljs', get_template_directory_uri() . '/js/global.js', array('jquery'), '1.0', TRUE);
-}
-
-
-if (!is_admin()) {
-    add_action('wp_enqueue_scripts', 'urframework_enqueue_jquery');
-}
-
 add_action( 'wp_enqueue_scripts', 'custom_scripts' );
+add_action( 'wp_enqueue_scripts', 'enqueue_jquery' );
 
 locate_template('functions/custom-post-types.php', TRUE);
 
